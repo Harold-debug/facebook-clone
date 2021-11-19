133dbeebfb0519cb42194c5b3e8c34a2d45378ae
Harold-debug <geumtchengh@gmail.com>
2021-11-19 23:52:48 +0100

    going forward with the header. Now doing header right

--- components/Header.js
+++ components/Header.js
@@ -16,7 +16,7 @@ import {
 import HeaderIcon from "./HeaderIcon";
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* left */}
 
       <div className="flex items-center">
@@ -30,7 +30,8 @@ function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500
+            flex-shrink "
             placeholder="Search Facebook"
             type="text"
           />
@@ -38,11 +39,21 @@ function Header() {
       </div>
       {/* center */}
       <div className="flex justify-center flex-grow ">
-        <div className="">
-          <HeaderIcon Icon={HomeIcon} />
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
         </div>
       </div>
       {/* right */}
+      <div>
+        {/* profile pic */}
+        <p className="whitespace-nowrap font-semibold pr-3">
+          Harold Geumtcheng
+        </p>
+      </div>
     </div>
   );
 }
