fe51a06fb051da7ad150a003478ee0e95b075850
Harold-debug <geumtchengh@gmail.com>
2021-11-21 19:58:56 +0100

    adding facebook login

---
+++ components/Login.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+function Login() {
+  return (
+    <div className="grid place-items-center p-10">
+      <Image
+        src="https://links.papareact.com/5me"
+        height={200}
+        width={200}
+        objectFit="contain"
+      />
+      <h1
+        className="p-5 m-20 bg-blue-500 rounded-full text-white
+          text-center cursor-pointer"
+      >
+        Login with Facebook
+      </h1>
+    </div>
+  );
+}
+
+export default Login;
