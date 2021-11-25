9d0acda9cebaf01a6bfdb96b85a30c5c550cc65b
Harold-debug <geumtchengh@gmail.com>
2021-11-25 19:35:18 +0100

    Update InputBox.js

--- components/InputBox.js
+++ components/InputBox.js
@@ -32,6 +32,12 @@ function InputBox() {
     if (e.target.files[0]) {
       reader.readAsDataURL(e.target.files[0]);
     }
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target.result);
+    };
+  };
+  const removeImage = () => {
+    setImageToPost(null);
   };
 
   return (
@@ -55,6 +61,15 @@ function InputBox() {
             Submit
           </button>
         </form>
+        {imageToPost && (
+          <div
+            onClick={removeImage}
+            className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer"
+          >
+            <img src={imageToPost} className="h-10 object-contain" alt="" />
+            <p className="text-xs text-red-500 text-center">Remove</p>
+          </div>
+        )}
       </div>
       <div className="flex justify-evenly p-3 border-t">
         <div className="inputIcon">
